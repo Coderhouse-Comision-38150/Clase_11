4926758839fdad905d5bdf80020792f04f23fe68
jpromanonet <programming@jpromano.net>
2022-12-14 20:54:36 -0300

    Ejercicio 01

--- Ejercicio_01/server.js
+++ Ejercicio_01/server.js
@@ -13,3 +13,8 @@ app.get('/datos', (req, res) => {
 })
 
 /* Configuramos el servidor */
+const PORT = 8080;
+const server = app.listen(PORT, () => {
+    console.log('Servidor corriendo en el puerto: ', PORT);
+})
+server.on('error', error => console.log('Error en el servidor: ', error));
