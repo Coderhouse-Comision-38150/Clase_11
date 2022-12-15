69fba727598d6880df093daa1cb834445e77f27c
jpromanonet <programming@jpromano.net>
2022-12-14 21:18:24 -0300

    Ejercicio 02

---
+++ Ejercicio_02/server.js
@@ -0,0 +1,20 @@
+/* Importando las depedencias */
+import express from 'express'
+
+/* Instanciar nuestras constantes */
+const app = express();
+
+/* Funcionalidades del servidor */
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.get('/datos', (req, res) => {
+    res.render('nivel', req.query);
+})
+
+/* Configuramos el servidor */
+const PORT = 8080;
+const server = app.listen(PORT, () => {
+    console.log('Servidor corriendo en el puerto: ', PORT);
+})
+server.on('error', error => console.log('Error en el servidor: ', error));
